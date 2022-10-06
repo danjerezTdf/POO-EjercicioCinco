f99ad593bf76df165a8505936281afff3d2e4244
Your Name <dgj7785@gmail.com>
2022-10-06 09:38:37 -0300

    create_main_v1

---
+++ main.ts
@@ -0,0 +1,5 @@
+import { Coach } from './class/class_coach'
+import { SoccerPlayer } from './class/class_soccerPlayer'
+import { MassageTherapist } from './class/class_massageTherapist'
+import { TeamSoccer } from './class/class_teamSoccer'
+
